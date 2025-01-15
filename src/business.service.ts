906752ba5c099e8817a052aefe8a4d2d99fa2dc4
Dylan O'Driscoll <dylan.odriscoll@gmail.com>
2025-01-15 20:56:28 +0000

    implement requirements and validation

--- src/business.service.ts
+++ src/business.service.ts
@@ -2,16 +2,27 @@ import mysql from 'mysql2/promise';
 
 export default class BusinessService {
     private connection: mysql.Connection
+    private tableName = 'businesses'
     constructor(connection: mysql.Connection) {
         this.connection = connection
     }
 
     async listTable(tableName: string) {
         const [results, data] = await this.connection.query(`
-            SELECT * from ${tableName}
+            SELECT * from ${this.tableName}
             LIMIT 6
         `)
-        console.log(results)
+        return results
+    }
+
+    async getByDistance(long: string, lat: string, limit: string, type: string) {
+        const [results, data] = await this.connection.query(`
+            SELECT *, ST_Distance_Sphere(point(${long}, ${lat}), point(longitude, latitude)) as distance
+            FROM  ${this.tableName}
+            ${type ? `WHERE type='${type}'`:''}
+            ORDER BY distance ASC
+            ${limit ? 'LIMIT ' + limit : ''}
+        `)
         return results
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,37 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import BusinessService from './business.service';
+import { isFloat, isInt } from 'validator';
+interface DiscoveryRequestQuery {
+  lat: string;
+  long: string;
+  limit: string;
+  type: string;
+}
+
+const middlewares = {
+  discovery: (req: Request<{}, {}, {}, DiscoveryRequestQuery>, res: Response, next: any) => {
+    const {long, lat, limit} = req.query;
+    let passedValidation = true
+    if (!lat || !long) {
+      res.status(400).send("long and lat are required parameters");
+      passedValidation = false
+    }
+    if (passedValidation && !isFloat(long) || !isFloat(lat)) {
+      res.status(400).send("long and lat must be integers");
+      passedValidation = false
+    }
+    if (passedValidation && limit) {
+      if (!isInt(limit) || parseFloat(limit) < 1) {
+        res.status(400).send("limit must be a positive integer");
+        passedValidation = false
+      }
+    }
+
+    if(passedValidation) {
+      next();
+    }
+  }
+}
 
 export function createRoutes(businessService: BusinessService) {
   const routes = Router();
@@ -9,6 +41,15 @@ export function createRoutes(businessService: BusinessService) {
     console.log(result)
     res.send({result})
   });
+
+  routes.get('/discovery', middlewares.discovery, async (
+    req: Request<{}, {}, {}, DiscoveryRequestQuery>,
+    res: Response
+  ) => {
+    const {long, lat, limit, type} = req.query;
+    const result = await businessService.getByDistance(long, lat, limit, type);
+    res.send({businesses: result})
+  })
 
   return routes
 }
